0fb29589f66fe22a0ff9de9b5e19e09a17d98db9
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-11-30 20:01:35 +0500

    Add files via upload

---
+++ 30-11-2024/app.js
@@ -0,0 +1,19 @@
+// var parentEle = document.getElementById("parent")
+// console.log(parentEle.childNodes);
+
+
+// var childEle = document.getElementById("child")
+// console.log(childEle.parentNode);
+
+// var parentEle = document.getElementById("parent")
+// console.log(parentEle.children);
+
+
+
+// var para = document.getElementsByTagName("p")
+// console.log(para);
+
+// let allParagraphs = document.getElementsByTagName("p");
+// console.log("Total paragraphs:", allParagraphs.length);
+
+
